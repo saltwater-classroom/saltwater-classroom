0fcd9ba6acbe09bd40054e74e692e43c0e8665a8
ajagow <budgiejag@gmail.com>
2019-12-16 13:07:23 -0500

    updated search functionality

--- components/shared_components/SearchBar.js
+++ components/shared_components/SearchBar.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import { View } from 'react-native';
-import { Item, Input, Icon, Button, Text } from 'native-base';
+import { Item, Input, Icon } from 'native-base';
 
 export default class SearchBar extends Component {
   render() {
     return (
       <View>
-        <Item>
-          <Input placeholder="Search" />
-          <Icon name="ios-search" />
+        <Item onPress={this.props.onClose}>
+          <Input placeholder="Search" onFocus={this.props.onFocus} onBlur={this.props.onBlur} />
+          <Icon name="ios-close" />
         </Item>
-        <Button transparent>
-          <Text>Search</Text>
-        </Button>
       </View>
     );
   }
 }
+
+const defaultOnFocus = () => console.log('focus');
+const defaultOnBlur = () => console.log('blur');
+const defaultOnClose = () => console.log('close');
+
+SearchBar.defaultProps = {
+  onClose: defaultOnClose,
+  onFocus: defaultOnFocus,
+  onBlur: defaultOnBlur
+};
+
+SearchBar.propTypes = {
+  onClose: PropTypes.func,
+  onFocus: PropTypes.func,
+  onBlur: PropTypes.func
+};
